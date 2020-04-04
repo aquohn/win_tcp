3d6d7c60e4381f21a78cdf1ee0bc35557bf7af5b
John Khoo <john_khoo@u.nus.edu>
2020-04-04 13:49:10 +0800

    Begin writing request parser

--- src/client_p.c
+++ src/client_p.c
@@ -6,13 +6,15 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define ERRPRINT(MSG, CODE) printf(MSG " Error code: %d\n", CODE); exit(1);
+#define WT_DIE(MSG, CODE) printf(MSG " Error code: %d\n", CODE); exit(1);
+#define WT_QUIT(MSG, CODE) close_serv(); WT_DIE(MSG, CODE);
+#define WT_DIE(MSG, CODE) printf(MSG " Error code: %d\n", CODE); exit(1);
 
 #define DEFAULT_BUFLEN 16
 #define DEFAULT_PORT 33333
 #define ADDR_BUFLEN 32
 #define SERV_IP_ADDR "127.0.0.1"
-#define USR "usr/"
+#define USR "usr"
 
 // compile with -lws2_32 at the END of the command
 
@@ -38,7 +40,7 @@ int main(int argc, char** argv) {
   // start version 2.2 of winsock
   int status = WSAStartup(MAKEWORD(2,2), &wsa); 
   if (status != 0) {
-    ERRPRINT("Failed to start winsock!", status);
+    WT_DIE("Failed to start winsock!", status);
   }
   SetConsoleCtrlHandler(int_handler, TRUE); // install Ctrl-C handler
 
@@ -46,7 +48,7 @@ int main(int argc, char** argv) {
   // TCP
   if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
     status = WSAGetLastError();
-    ERRPRINT("Failed to create socket!", status);
+    WT_DIE("Failed to create socket!", status);
   }
 
   // Specify the server to connect to
@@ -63,14 +65,14 @@ int main(int argc, char** argv) {
   // Connect
   if (connect(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
     status = WSAGetLastError();
-    ERRPRINT("Failed to connect to server!", status);
+    WT_DIE("Failed to connect to server!", status);
   }
 
   // Send some text
   char buf[DEFAULT_BUFLEN] = "Get served lmao";
   if (send(sock, buf, strlen(buf), 0) < 0) {
     status = WSAGetLastError();
-    ERRPRINT("Failed to send data!", status);
+    WT_DIE("Failed to send data!", status);
   }
 
   close_cli();
--- src/server.c
+++ src/server.c
@@ -6,41 +6,42 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define ERRPRINT(MSG, CODE) printf(MSG " Error code: %d\n", CODE); exit(1);
+#define WT_DIE(MSG, CODE) printf("%s Error code: %d\n", MSG, CODE); exit(1)
+#define WT_QUIT(MSG, CODE) close_serv(); WT_DIE(MSG, CODE)
 
 #define DEFAULT_BUFLEN 1024
 #define DEFAULT_PORT 33333
 #define ADDR_BUFLEN 32
 #define BACKLOG 5
-#define SRV "srv/"
+
+// some Windows macro I'm not using
+#ifdef DELETE
+  #undef DELETE
+#endif
+#define HTTP_HDR_END "\r\n\r\n"
+#define SRV "srv"
+
+enum http_mtd {GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH,
+MTD_COUNT}; // for keeping track of number of methods
+char *http_mtd_strs[] = {"GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT",
+  "OPTIONS", "TRACE", "PATCH"};
+
+void close_serv();
+BOOL WINAPI int_handler(DWORD sig_type);
+FILE * handle_req(const char *req, enum http_mtd *mtd, char **data, 
+    char **errmsg);
 
 // compile with -lws2_32 at the END of the command
 
 SOCKET listen_sock = INVALID_SOCKET; // for receiving connections
 SOCKET conn_sock = INVALID_SOCKET; // for maintaining a connection
 
-void close_serv() {
-  printf("\nClosing sockets.\n");
-  shutdown(listen_sock, SD_BOTH);
-  shutdown(conn_sock, SD_BOTH);
-  closesocket(listen_sock);
-  closesocket(conn_sock);
-  WSACleanup();
-}
-
-BOOL WINAPI int_handler(DWORD sig_type) {
-  if (sig_type == CTRL_C_EVENT || sig_type == CTRL_BREAK_EVENT) {
-    close_serv();
-  }
-  return FALSE;
-}
-
 int main(int argc, char** argv) {
   WSADATA wsa;
   // start version 2.2 of winsock
   int status = WSAStartup(MAKEWORD(2,2), &wsa); 
   if (status != 0) {
-    ERRPRINT("Failed to start winsock!", status);
+    WT_DIE("Failed to start winsock!", status);
   }
   SetConsoleCtrlHandler(int_handler, TRUE); // install Ctrl-C handler
 
@@ -48,7 +49,7 @@ int main(int argc, char** argv) {
   // TCP
   if ((listen_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
     status = WSAGetLastError();
-    ERRPRINT("Failed to create listening socket!", status);
+    WT_DIE("Failed to create listening socket!", status);
   }
 
   // Set up server address object
@@ -63,13 +64,13 @@ int main(int argc, char** argv) {
   // Bind socket to address
   if (bind(listen_sock, (struct sockaddr *) &serv_addr, sin_size) < 0) {
     status = WSAGetLastError();
-    ERRPRINT("Failed to bind listening socket!", status);
+    WT_DIE("Failed to bind listening socket!", status);
   }
 
   // Start listening
   if (listen(listen_sock, BACKLOG) < 0) {
     status = WSAGetLastError();
-    ERRPRINT("Failed to start listening!", status);
+    WT_DIE("Failed to start listening!", status);
   } 
 
   printf("Server listening on port %u!\n\n", DEFAULT_PORT);
@@ -82,24 +83,98 @@ int main(int argc, char** argv) {
     char cli_addr_str[ADDR_BUFLEN];
     inet_ntop(AF_INET, &cli_addr.sin_addr, cli_addr_str, ADDR_BUFLEN);
     printf("Connection from %s\n", cli_addr_str);
+    enum http_mtd req_mtd;
 
-    // read and print data
+    // Read in data
     int conn_status = SOCKET_ERROR;
     char recvbuf[DEFAULT_BUFLEN + 1];
     size_t recvbuflen = DEFAULT_BUFLEN;
     do {
       conn_status = recv(conn_sock, recvbuf, (int) recvbuflen, 0);
       if (conn_status == SOCKET_ERROR) {
-        ERRPRINT("Error receiving data!", WSAGetLastError());
+        WT_QUIT("Error receiving data!", WSAGetLastError());
       } else if (conn_status == 0) {
         printf("Connection closed by client.\n");
       } else {
-        printf("Received %d bytes.\n", conn_status);
+        printf("Read %d bytes.\n", conn_status);
         recvbuf[conn_status] = 0;
-        printf("%s\n", recvbuf);
+
+        char req_err[DEFAULT_BUFLEN];
+        char *req_data;
+        FILE *serv_file = NULL;
+        // establish request type
+        if (serv_file == NULL) {
+          serv_file = handle_req(recvbuf, &req_mtd, &req_data, 
+              (char **) &req_err);
+          // something went wrong when prcoessing the header
+          if (serv_file == NULL) {
+            WT_QUIT(req_err, 1);
+          }
+        }
+        
+        // only implementing GET for this assignment
+        
+
       }
     } while (conn_status != 0);
   }
-
   return 0;
 }  
+
+/**
+ * Parse a HTTP 1.1 request, returning a file handler to the relevant file that
+ * needs to be served, indicating the HTTP method used, the position where the
+ * data starts, and an error message string.
+ */
+FILE * handle_req(const char *req, enum http_mtd *mtd, char **data, 
+    char **errmsg) {
+
+  // check validity and mark start of data
+  char *end = strstr(req, HTTP_HDR_END);
+  if (end == NULL) {
+    sprintf(*errmsg, "Malformed HTTP header!");
+    return NULL;
+  }
+  *data = end + sizeof(HTTP_HDR_END);
+
+  // identify type of request
+  // can be made marginally more efficient by keying the methods on length
+  size_t typelen = strcspn(req, " ");
+  int i;
+  for (i = 0; i < (int) MTD_COUNT; ++i) {
+    if (strncmp(req, http_mtd_strs[i], typelen) == 0) {
+      *mtd = (enum http_mtd) i;
+      break;
+    }
+  }
+  if (i == (int) MTD_COUNT) {
+    sprintf(*errmsg, "No such HTTP method!");
+    return NULL;
+  }
+
+  // only implementing GET for specified filetypes for assignment
+
+  return NULL;
+}
+
+/**
+ * Interrupt handler for Ctrl-C or window close events.
+ */
+BOOL WINAPI int_handler(DWORD sig_type) {
+  if (sig_type == CTRL_C_EVENT || sig_type == CTRL_BREAK_EVENT) {
+    close_serv();
+  }
+  return FALSE;
+}
+
+/**
+ * Close down server connections and clean up.
+ */
+void close_serv() {
+  printf("\nClosing sockets.\n");
+  shutdown(listen_sock, SD_BOTH);
+  shutdown(conn_sock, SD_BOTH);
+  closesocket(listen_sock);
+  closesocket(conn_sock);
+  WSACleanup();
+}
